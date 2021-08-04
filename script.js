44c59e5eb53f1bba8acf53289f0c825bdb0b19e2
unknown <kevinaborders@gmail.com>
2021-08-03 21:05:08 -0500

    completed studio

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+
+            
+
+            json.sort(function (a, b) {
+                return b.hoursInSpace - a.hoursInSpace;
+            });
+
+            const astronautCount = document.getElementById("count");
+            astronautCount.innerHTML += `: ${json.length}`;
+            const container = document.getElementById("container");
+
+            for (let i = 0; i < json.length; i++) {
+            
+                let skills = json[i].skills.join(", ");
+                
+                container.innerHTML += `
+                    <div class ="astronaut">
+                            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                <li id="activeStatus${json[i].id}">Active: ${json[i].active}</li>
+                                <li>Skills: ${skills}</li>
+                            </ul>
+                        <img class="avatar" src=${json[i].picture}>
+                    </div>
+                    `;
+
+                if (json[i].active === true) {
+                    document.querySelector(`#activeStatus${json[i].id}`).style.color =
+                        "green";
+                }
+              
+            };
+
+            
+        });
+    });
+    
+    
+});
